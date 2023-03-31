40cc1f14e196061d906c873ba8eae6834d8c9953
KnightBritomart <108755072+MadisonAiman@users.noreply.github.com>
2023-03-31 08:53:15 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+//In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+function makeNegative(num){
+ return num > 0 ? num * -1 : num;
+  }
+
+//makeNegative(1);    // return -1
+//makeNegative(-5);   // return -5
+//makeNegative(0);    // return 0
+//makeNegative(0.12); // return -0.12
+
+
+
+
 //Create a function that will convert boolean value to its string representation 
 
 function booleanToString(b){
