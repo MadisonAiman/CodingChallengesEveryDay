5e34b066a902f0d2830f9905e16b205e2660cd57
KnightBritomart <108755072+MadisonAiman@users.noreply.github.com>
2023-04-03 10:08:01 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+//Complete the solution so that it reverses the string passed into it. 
+
+//'cat' => 'tac'
+//'candle' => 'eldnac'
+
+function solution(str){
+    return str.split("").reverse().join("");
+}
+
+
+
 //Complete the square sum function so that it squares each number passed into it and then sums the results together.
 
 //For example, for [1, 2, 2] it should return 9 because 12+22+22=91^2 + 2^2 + 2^2 = 912+22+22=9.
