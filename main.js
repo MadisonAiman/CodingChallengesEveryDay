a79741a15a0fa2b89aaffa0bffc5ac3f56160bc6
KnightBritomart <108755072+MadisonAiman@users.noreply.github.com>
2023-04-07 09:46:56 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+//Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+function cockroachSpeed(s) {
+  //multiply km/m by 27.7777777778
+  return Math.floor(s*27.7777777778);
+}
+
 //Double the integer 
 
 function doubleInt(i){
