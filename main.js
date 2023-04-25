3509ca61d3d4aea8c9a468f7e4f936dd1315c354
KnightBritomart <108755072+MadisonAiman@users.noreply.github.com>
2023-04-24 21:24:44 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-//streams
+
 
 // Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
 //Don't change the order of the elements that are left.
-//Examples
+//Examples; these are what you can wrape in console.log(), true
 
 //* Input: [1,2,3,4,5], output = [2,3,4,5]
 //* Input: [5,3,2,1,4], output = [5,3,2,4]
