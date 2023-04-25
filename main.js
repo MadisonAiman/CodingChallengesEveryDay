9b0c1a056c1b81690ec9710e63010797e6314839
KnightBritomart <108755072+MadisonAiman@users.noreply.github.com>
2023-04-25 09:33:17 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
+//Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+//Examples (input -> output)
+
+//6, "I"     -> "IIIIII"
+//5, "Hello" -> "HelloHelloHelloHelloHello"
+
+
+
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
+
+//By using the repeat method, it will return S the same number of time that N is passed through
+
+//or with a simple for loop
+
+
+function repeatStr (n, s) {
+  var text = '';
+  for(var i = 0; i < n; ++i){
+    text += s;
+  }
+  return text;
+}
+
 
 
 //Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
