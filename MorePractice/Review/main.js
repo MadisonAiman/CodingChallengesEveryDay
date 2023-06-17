247b57b29fc1cbd33b704bc3b48afca554ee8f53
Madison Aiman <108755072+MadisonAiman@users.noreply.github.com>
2023-06-17 12:31:53 -0500

    Update main.js

--- MorePractice/Review/main.js
+++ MorePractice/Review/main.js
@@ -1,3 +1,19 @@
+//
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    let smallest = args[0];
+
+    for (let i = 1; i < args.length; i++) {
+      if (args[i] < smallest) {
+        smallest = args[i];
+      }
+    }
+
+    return smallest;
+  }
+}
+
 
 //Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
 
