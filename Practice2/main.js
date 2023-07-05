3669088c48036a7ed9fef789f6fc6ad413d167fd
Madison Aiman <108755072+MadisonAiman@users.noreply.github.com>
2023-07-05 13:51:02 -0500

    Update main.js

--- Practice2/main.js
+++ Practice2/main.js
@@ -74,7 +74,7 @@ function removeChar(str){
 
 //Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
 function cockroachSpeed(s) {
-  //multiply km/m by 27.7777777778
+  //multiply km/m by 27.7777777778 easy
   return Math.floor(s*27.7777777778);
 }
 
